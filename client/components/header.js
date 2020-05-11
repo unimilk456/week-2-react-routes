3c6792776d90970d517f7790cec847d0f7de8dbf
Mikhail-Mac <unimilk456@gmail.com>
2020-05-11 17:41:23 +0300

    Fixed ref to Google

--- client/components/header.js
+++ client/components/header.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 
 const Header = () => {
   const [toggled, toggle] = useState(false)
-
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
@@ -26,10 +25,8 @@ const Header = () => {
       </div>
       {toggled && (
         <div id="menu" className="block">
-          <button
-            type="button"
-            // eslint-disable-next-line react/no-string-refs
-            ref="https://www.google.com/"
+          <a
+            href="https://www.google.com/"
             className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
           >
             <svg
@@ -40,7 +37,7 @@ const Header = () => {
               <title>Menu</title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
-          </button>
+          </a>
         </div>
       )}
     </nav>
